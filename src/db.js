8c07b3c746e878f985652934a086c69efac50dcc
hieutrluu <thl1g15@outlook.com>
2023-03-17 02:10:05 +0000

    add frappe-js-sdk and using javascript frappe.db instead of pg-promise

--- src/db.js
+++ src/db.js
@@ -1,11 +1,23 @@
 import pgInit from 'pg-promise';
+import { FrappeApp } from "frappe-js-sdk";
 
-const pgp = pgInit();
-export const db = pgp("postgresql://pgvectortest:123@pgvector:5432/pgvectortest");
+const frappe = new FrappeApp("https://localhost:8000", {
+    useToken: true,
+    // Pass a custom function that returns the token as a string - this could be fetched from LocalStorage or auth providers like Firebase, Auth0 etc.
+    token: getTokenFromLocalStorage(),
+    // This can be "Bearer" or "token"
+    type: "Bearer"
+})
 
-const {isolationLevel} = pgp.txMode;
+// const pgp = pgInit();
+// export const db = pgp("postgresql://pgvectortest:123@pgvector:5432/pgvectortest");
+
+// const {isolationLevel} = pgp.txMode;
 
 // Helper to make sure we always access database at serializable level.
 export async function tx(f) {
-  return await db.tx({mode: isolationLevel.serializable}, f);
+  frappe.db.sql("SET TRANSACTION ISOLATION LEVEL READ COMMITTED")
+  return await frappe.db(f);
 }
+
+// Replace using pg-promise (working directly with the database) to working with the frappe-js-sdk or frappe-react-sdk
